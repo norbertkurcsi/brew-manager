d991fa44d996b4188795c574fa81df511cb1d8db
Kurcsi Norbert <kurcsi.norbert@edu.bme.hu>
2024-04-24 15:56:38 +0200

    add converter for below threshold ingredients

---
+++ winui/BrewManager/BrewManager/Converters/StockThresholdToBrushConverter.cs
@@ -0,0 +1,22 @@
+﻿using BrewManager.Core.Models;
+using Microsoft.UI;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Media;
+
+
+
+namespace BrewManager.Converters;
+
+public class StockThresholdToBrushConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is Ingredient ingredient)
+        {
+            return ingredient.Stock < ingredient.Threshold ? new SolidColorBrush() { Color = Colors.PaleVioletRed } : new SolidColorBrush() { Color = Colors.Transparent };
+        }
+
+        return new SolidColorBrush() { Color = Colors.Transparent };
+    }
+    public object ConvertBack(object value, Type targetType, object parameter, string language) => throw new NotImplementedException();
+}
