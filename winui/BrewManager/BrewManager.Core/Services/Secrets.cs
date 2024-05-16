5ffc424943a770cbcf0d432e4d563a7c21565fcd
Kurcsi Norbert <90260479+norbertkurcsi@users.noreply.github.com>
2024-05-16 20:32:01 +0200

    Update Secrets.cs

--- winui/BrewManager/BrewManager.Core/Services/Secrets.cs
+++ winui/BrewManager/BrewManager.Core/Services/Secrets.cs
@@ -4,5 +4,5 @@ public class Secrets
 {
     public static string BaseUrl => "https://brewmanager-backend.azurewebsites.net";
 
-    public static string StorageConnectionString => "DefaultEndpointsProtocol=https;AccountName=brewmanager;AccountKey=dm1GhQCQjnOHhNQWOtHL0lIvK3FH4d6Z35HiF38D2q5m7Xjub2LHFZNGxMaDzaxs9rGFc0FjKOSf+ASt1z0lgg==;EndpointSuffix=core.windows.net";
+    public static string StorageConnectionString => "secret";
 }
